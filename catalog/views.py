f650c8cb9ff0383b20206d9f8b5641cc0474d667
Omar Tall <111260004+Mr-Tall@users.noreply.github.com>
2025-12-24 19:25:22 -0500

    Add public pages: home and anime detail

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Anime
 
-# Create your views here.
+def home(request):
+    anime_list = Anime.objects.order_by("title")
+    return render(request, "catalog/home.html", {"anime_list": anime_list})
+
+
+def anime_detail(request, slug):
+    anime = get_object_or_404(Anime, slug=slug)
+    characters = anime.characters.order_by("name")
+    return render(request, "catalog/anime_detail.html", {"anime": anime, "characters": characters})
